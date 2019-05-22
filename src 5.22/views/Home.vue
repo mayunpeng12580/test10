01cb619ff50baa136af7e042a2b3af996c584a61
mayunpeng <2469000189@qq.com>
2019-05-22 22:51:05 +0800

    1

---
+++ src 5.22/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+  	<search-bar></search-bar>
+  	
+  	<swiper></swiper>
+  	<quick-nav></quick-nav>
+  	
+  	<product-list></product-list>
+  		
+  		
+  
+  	
+  	
+  	<div style="height: 1000px;"></div>
+  	
+  
+  	
+  	<nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+
+import SearchBar from "@/components/SearchBar";
+import Swiper from "@/components/HomeSwiper";
+import QuickNav from "@/components/QuickNav";
+import NavBar from "@/components/NavBar";
+import ProductList from "@/components/ProductList";
+
+export default {
+  name: 'home',
+  components: {
+  	SearchBar,
+  	Swiper,
+  	QuickNav,
+  	NavBar,
+  	ProductList
+  }
+}
+</script>
+
+<style >
+	
+	.box{
+		width: 200px;
+		height: 200px;
+		background-color: red; 	
+	}
+	 
+</style>
